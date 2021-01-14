2002fd294f3734133fd3af62563d6c8b4db35966
Riwandy <riwandys@gmail.com>
2021-01-14 17:06:42 +0700

    Displaying comments

--- blog/client/src/CommentCreate.js
+++ blog/client/src/CommentCreate.js
@@ -1,30 +1,32 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import axios from 'axios';
 
 export default ({ postId }) => {
     const [content, setContent] = useState('');
 
-    const onSubmit = async (event) => {
+    const onSubmit = async event => {
         event.preventDefault();
 
-        await axios.post(`https://localhost:4001/posts/${postId}/comments`, {
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
             content
         });
 
         setContent('');
-    }
+    };
 
-    return <div>
-        <form onSubmit={onSubmit}>
-            <div className="form-group">
-                <label>New Comment</label>
-                <input
-                    value="content"
-                    onChange={e => setContent(e.target.value)}
-                    className="form-control"
-                />
-            </div>
-            <button className="btn btn-primary">Submit</button>
-        </form>
-    </div>;
-}
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label>New Comment</label>
+                    <input
+                        value={content}
+                        onChange={e => setContent(e.target.value)}
+                        className="form-control"
+                    />
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    );
+};
--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { render } from 'react-dom';
 import CommentCreate from './CommentCreate';
+import CommentList from "./CommentList";
 
 export default () => {
     const [posts, setPosts] = useState({});
+
     const fetchPosts = async () => {
         const res = await axios.get('http://localhost:4000/posts');
 
@@ -19,18 +20,21 @@ export default () => {
         return (
             <div
                 className="card"
-                style={{width:'30%', marginBottom:'20px'}}
+                style={{ width: '30%', marginBottom: '20px' }}
                 key={post.id}
             >
                 <div className="card-body">
                     <h3>{post.title}</h3>
+                    <CommentList postId={post.id} />
                     <CommentCreate postId={post.id} />
                 </div>
             </div>
-        )
-    })
+        );
+    });
 
-    return <div className="d-flex flex-row flex-wrap justify-content-between">
-        {renderedPosts}
-    </div>;
+    return (
+        <div className="d-flex flex-row flex-wrap justify-content-between">
+            {renderedPosts}
+        </div>
+    );
 };
